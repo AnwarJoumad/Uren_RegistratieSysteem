724b2ad9d3c81e794dc6edd321383fca54983ac7
ThomasvanGastel2 <71122683+ThomasvanGastel2@users.noreply.github.com>
2022-12-08 16:22:55 +0100

    styling create pagina's

--- resources/views/works/create.blade.php
+++ resources/views/works/create.blade.php
@@ -1,36 +1,54 @@
-<form action="{{route('works.store')}}" method="POST">
-    @csrf
-<label for="start_time">start dag</label><br>
-<input type="datetime-local" id="start_time" name="start_time" ><br>
+<livewire:side-menu />
 
-<label for="end_time">einde dag</label><br>
-<input type="datetime-local" id="end_time" name="end_time" ><br>
+<x-app-layout>
 
-    <label>Aantal minuten pauze</label> <br>
-        <select name="break" >
-            <option value="0.00">0 minuten pauze</option>
-            <option value="0.25">15 minuten pauze</option>
-            <option value="0.5">30 minuten pauze</option>
-            <option value="0.75">45 minuten pauze</option>
-            <option value="1.00">60 minuten pauze</option>
-            <option value="1.25">75 minuten pauze</option>
-            <option value="1.50">90 minuten pauze</option>
-        </select>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Verlof
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <form action="{{route('verlof.store')}}" method="POST">
+                        @csrf
+                        <div class="grid md:grid-cols-2 md:gap-6">
+                            <div class="relative z-0  w-full group">
+                                <label for="start_time">Start dag</label><br>
+                                <input type="datetime-local" id="start_datetime" class="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="start_datetime" ><br>
+                            </div>
+                            <div class="relative z-0  w-full group">
+                                <label for="end_time">Einde dag</label><br>
+                                <input type="datetime-local" id="end_datetime" class="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="end_datetime" ><br>
+                            </div>
+                        </div>
+                        <label >Aantal minuten pauze</label><br>
+                        <select name="break" class="bg-gray-50 border border-gray-300 mt-2text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+                            <option value="0.00">0 minuten pauze</option>
+                            <option value="0.25">15 minuten pauze</option>
+                            <option value="0.5">30 minuten pauze</option>
+                            <option value="0.75">45 minuten pauze</option>
+                            <option value="1.00">60 minuten pauze</option>
+                            <option value="1.25">75 minuten pauze</option>
+                            <option value="1.50">90 minuten pauze</option>
+                        </select>
 
-    <br><label>gewerkte taak</label> <br>
-    <select name="task" >
-@foreach($tasks as $task)
-            <option value="{{$task->id}}">{{$task->name}}</option>
-        @endforeach
-    </select>
+                        <br><label>Gewerkte taak</label> <br>
+                        <select name="task" class="bg-gray-50 border border-gray-300 mb-3 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+                            @foreach($tasks as $task)
+                                <option value="{{$task->id}}">{{$task->name}}</option>
+                            @endforeach
+                        </select>
+                        <label for="description">Omschrijving</label><br>
+                        <textarea id="description" class="block p-2.5 w-full mt-2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="description" rows="4" cols="50"></textarea><br>
 
-    <label for="description">reden</label><br>
-    <textarea id="description" name="description" rows="4" cols="50"></textarea><br>
+                        <button type="submit" style="background-color: #ee0d73;" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
 
 
-
-
-
-
-    <br><input type="submit" value="tijd registeren">
-</form>
