9dcf51fb792f36b4da36a0a6c77c45271f047488
AnwarJoumad <71122375+AnwarJoumad@users.noreply.github.com>
2022-12-15 16:44:10 +0100

    text fix

--- resources/views/works/create.blade.php
+++ resources/views/works/create.blade.php
@@ -18,7 +18,7 @@
                             </div>
                         </div>
                         <label >Aantal minuten pauze</label><br>
-                        <select name="break" class="bg-gray-50 border border-gray-300 mt-2 text-black-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+                        <select name="break" class="bg-gray-50 border border-gray-300 mt-2 text-black-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" >
                             <option value="0.00">0 minuten </option>
                             <option value="0.25">15 minuten</option>
                             <option value="0.5">30 minuten</option>
--- resources/views/works/edit.blade.php
+++ resources/views/works/edit.blade.php
@@ -22,7 +22,7 @@
                             </div>
                         </div>
                         <label >Aantal minuten pauze</label><br>
-                        <select name="break" class="bg-gray-50 border border-gray-300 mt-2 text-black-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+                        <select name="break" class="bg-gray-50 border border-gray-300 mt-2 text-black-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " >
                             <option value="{{$timework->break_time}}"> {{$timework->break_time * 60}} minuten </option>
                             <option value="0.00">0 minuten </option>
                             <option value="0.25">15 minuten</option>
