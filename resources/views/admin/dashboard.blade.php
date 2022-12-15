137ca3fbbd70363cd5a73d31d47f4d7494d811d5
mickjacobs2004 <D283803@edu.rocwb.nl>
2022-12-15 16:40:44 +0100

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,10 +1,6 @@
 <x-app-layout>
     @section('content')
-            <x-slot name="header">
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                    Admin Dashboard
-                </h2>
-            </x-slot>
+
         <div class="py-12 mb-10">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
