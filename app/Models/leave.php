e3ce1430a36bb3ef6b5970d7aacf1687d4e9a2b6
AnwarJoumad <71122375+AnwarJoumad@users.noreply.github.com>
2022-12-15 16:11:25 +0100

    fix pagina

--- app/Models/leave.php
+++ app/Models/leave.php
@@ -10,4 +10,12 @@ class leave extends Model
     use HasFactory;
     protected $table = 'verlof';
 
+<<<<<<< Updated upstream
+=======
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+>>>>>>> Stashed changes
 }
