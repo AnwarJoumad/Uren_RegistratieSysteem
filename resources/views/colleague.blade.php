ca33bf88ca642206db97c572ac72d427c736e122
TjKalau <71122246+TjKalau@users.noreply.github.com>
2023-01-26 14:04:33 +0100

    update colleague page

--- resources/views/colleague.blade.php
+++ resources/views/colleague.blade.php
@@ -3,47 +3,110 @@
     <x-slot name="header">
         <script src="https://code.jquery.com/jquery-3.6.1.slim.min.js" integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA=" crossorigin="anonymous"></script>
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css">
-
-        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.js"></script>
     </x-slot>
-    <div class="py-12 mb-10">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-8 text-gray-900">
 
-                    <div class="overflow-x-auto relative">
-                        <h1 class="font-semibold text-xl">Mijn gegevens</h1>
-                    </div>
-                </div>
+    <div style="display: flex; justify-content: space-around; margin-left: 50px">
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" style="margin-left: 80px">
+            <a href="#">
+                <img class="rounded-t-lg" src="https://source.unsplash.com/random/350x200?sig=1" alt="" />
+            </a>
+            <div class="p-5" style="width: 350px">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Tj Kalau</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><span style="font-weight: bold"> Functie:</span> Admin</p>
+                <a href="mailto:tjkalau4@gmail.com" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Stuur mail
+                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
             </div>
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-4">
-                <div class="p-8 text-gray-900">
-                    <div class="overflow-x-auto relative">
-                        <table  id="myTable" class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="py-3 px-6">
-                                    Datum
-                                </th>
-                                <th scope="col" class="py-3 px-6">
-                                    Gewerkte uren
-                                </th>
-                                <th scope="col" class="py-3 px-6">
-                                    Pauze
-                                </th>
-                                <th scope="col" class="py-3 px-6">
-                                    activiteit
-                                </th>
-                            </tr>
-                            </thead>
-                            <tbody>
+        </div>
 
-                            </tbody>
-                        </table>
-                    </div>
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <img class="rounded-t-lg" src="https://source.unsplash.com/random/350x200?sig=1" alt="" />
+            </a>
+            <div class="p-5" style="width: 350px">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Anwar Joumad</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><span style="font-weight: bold"> Functie:</span> Admin</p>
+                <a href="mailto:anwarjoumad10@gmail.com" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more
+                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+            </div>
+        </div>
 
-                </div>
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" style="margin-right: 50px;">
+            <a href="#">
+                <img class="rounded-t-lg" src="https://source.unsplash.com/random/350x200?sig=1" alt="" />
+            </a>
+            <div class="p-5" style="width: 350px;">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Thomas van Gastel</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><span style="font-weight: bold"> Functie:</span> Admin</p>
+                <a href="mailto:thomasvgastel3@gmail.com" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more
+                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
             </div>
         </div>
     </div>
+
+
+    <div style="display: flex; justify-content: space-around; margin-left: 50px; margin-top: 50px; margin-bottom: 50px">
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" style="margin-left: 80px">
+            <a href="#">
+                <img class="rounded-t-lg" src="https://source.unsplash.com/random/350x200?sig=1" alt="" />
+            </a>
+            <div class="p-5" style="width: 350px">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Mick Jacobs</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><span style="font-weight: bold"> Functie:</span> Admin</p>
+                <a href="mailto:mickjacobs04@gmail.com" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Stuur mail
+                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+            </div>
+        </div>
+
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <img class="rounded-t-lg" src="https://source.unsplash.com/random/350x200?sig=1" alt="" />
+            </a>
+            <div class="p-5" style="width: 350px">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Pepijn van den Kieboom</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><span style="font-weight: bold"> Functie:</span> Admin</p>
+                <a href="mailto:pepijnvdkieboom@gmail.com" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more
+                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+            </div>
+        </div>
+
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" style="margin-right: 50px;">
+            <a href="#">
+                <img class="rounded-t-lg" src="https://source.unsplash.com/random/350x200?sig=1" alt="" />
+            </a>
+            <div class="p-5" style="width: 350px;">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Tj_test</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><span style="font-weight: bold"> Functie:</span> Werknemer</p>
+                <a href="mailto:test@test.com" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more
+                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+
+
 </x-app-layout>
